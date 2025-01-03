c165789ac06c329c9f5e22809f0cad5cb6af4aed
UdaykumarPedduri <125956095+UdaykumarPedduri@users.noreply.github.com>
2025-01-03 14:32:06 +0000

    commit1

---
+++ variable.tf
@@ -0,0 +1,13 @@
+variable "aws_region" {
+  description = "AWS region for resources"
+  type        = string
+  default     = "us-east-1"
+}
+
+variable "lambda_name" {
+  description = "Name of the Lambda function"
+  type        = string
+  default     = "ukp-gd_lambda"
+}
+
+
